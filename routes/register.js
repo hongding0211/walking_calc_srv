15ce4c7c87650d5dfaf3d368665a9d3bf1c05ff2
HongDing97 <keith.dh@hotmail.com>
2021-11-12 10:44:58 +0800

    对图片进行压缩处理

--- routes/register.js
+++ routes/register.js
@@ -4,44 +4,50 @@ const ResponseData = require('../module/ResponseData');
 const formidable = require('formidable')
 const DataBase = require('../module/db')
 const fs = require('fs')
-
+const images = require('images')
 /* Register */
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const db = new DataBase()
     const form = new formidable.IncomingForm()
-    result = 'fail'
+    let result = 'fail'
+    let imgSize = 0
+    let imgNewSize = 0
+
     form.parse(req, async (err, fields, files) => {
-        imgSize = 0
         if (!err) {
-            const db = new DataBase()
-            const img = files.avatar
-            imgSize = img.size
-            // file size limit
-            if (imgSize > 4096 * 1024) {
-                result = 'image size lager than 4mb'
-                return
-            }
-            const imgBase64 = imgFile2Base64(img)
             // find first, if not exists, then insert
             if ((await db.find('users', { uid: fields.uid })).length == 0) {
-                await db.insert('users', [{
-                    ...fields,
-                    img: imgBase64
-                }])
-                result = 'ok'
+                const img = files.avatar
+                imgSize = (img.size / 1024).toFixed(2)
+                // size limit
+                if (imgSize > 4096 * 1024) {
+                    result = 'image size lager than 4mb'
+                } else {
+                    const imgBase64 = imgFile2Base64(img)
+                    imgNewSize = (imgBase64.length / 1024).toFixed(2)
+                    await db.insert('users', [{
+                        ...fields,
+                        img: imgBase64
+                    }])
+                    result = 'ok'
+                }
             } else {
                 result = 'already exists'
             }
         }
-        res.send(new ResponseData({
+        await res.send(new ResponseData({
             ...fields,
-            img_size: `${imgSize / (1024)}kb`
+            img_origin_size: `${imgSize}kb`,
+            img_resized_size: `${imgNewSize}kb`
         }, result))
     })
 })
 
 const imgFile2Base64 = function (f) {
-    const bitmap = fs.readFileSync(f.filepath)
-    return Buffer.from(bitmap, 'base64')
+    const imgFile = fs.readFileSync(f.filepath)
+    // resize
+    const img = images(imgFile).size(120).encode('jpg', { operation: 50 })
+    return Buffer.from(img, 'base64')
 }
 
 module.exports = router;
