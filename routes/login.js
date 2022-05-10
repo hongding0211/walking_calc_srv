2b76c38e40829be36cf364a9ea72cd8d04716aed
HongDing97 <keith.dh@hotmail.com>
2022-05-10 19:33:14 +0800

    add login log

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 const ResponseData = require('../module/responseData');
 const DataBase = require('../module/dataBase')
+const log4js = require('log4js')
+
+log4js.configure({
+    appenders: { login: { type: "file", filename: "login.log" } },
+    categories: { default: { appenders: ["login"], level: "info" } }
+})
+
+function log(text) {
+  const logger = log4js.getLogger('login')
+  logger.info(text)
+}
 
 /* Login */
 router.get('/', async (req, res) => {
+    // log
+    const userAgent = req.headers['user-agent']
+    log(userAgent)
+
     const { uid } = req.query
     const db = new DataBase()
     const dbRes = await db.find('users', { uid })
